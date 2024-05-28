ac8e66cc83004499f515e5a921afcb82d5645761
rizkirobawa <rizkirobawa@gmail.com>
2024-05-28 21:52:37 +0700

    feat: add API payment gateway

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const Payment = require("../v1/payment.routes");
+
+router.use("/api/v1/payments", Payment);
 
 module.exports = router;
