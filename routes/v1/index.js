824aa9e13dca65b1b2d4c1c72257ae6951ce806b
Faiq Al Abiyyi <abiyyiaja@gmail.com>
2024-05-29 20:32:27 +0700

    feat: add booking routes

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const bookingController = require('../../controllers/booking.controllers');
-router.post('/booking', bookingController.booking);
-router.get('/booking-history', bookingController.getAll);
-router.get('/booking-history/:bookingId', bookingController.getDetail);
+const Booking = require('../v1/booking.routes');
+
+router.use('/api/v1/bookings', Booking);
 
 module.exports = router;
