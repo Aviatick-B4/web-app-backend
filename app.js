57177c5e3d026bd65fd914b3fd8157b7ae450d92
rizkirobawa <rizkirobawa@gmail.com>
2024-05-29 15:34:24 +0700

    feat: add middlewares cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 const routes = require('./routes/v1/index');
 
@@ -12,6 +13,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
