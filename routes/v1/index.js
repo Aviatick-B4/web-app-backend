3eef407bd24532242df386e2abf5f32a82001b31
rizkirobawa <rizkirobawa@gmail.com>
2024-05-30 22:15:09 +0700

    fix: prisma not defined

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 const ticketRoutes = require('./ticket.routes');
 const Payment = require("../v1/payment.routes");
 
