911c72cd654670164c8507cfd9a452ea1247c8d6
Rama Astra <ramaastra333@gmail.com>
2024-05-30 10:17:40 +0800

    feat: add query params filtering for flight search endpoint

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -3,9 +3,62 @@ const prisma = new PrismaClient();
 
 module.exports = {
   getAll: async (req, res, next) => {
+    const {
+      from: originCity,
+      to: destinationCity,
+      departure: departureDate,
+      return: returnDate,
+      passengers: passengersCount,
+      seat_class: seatClass
+    } = req.query;
+
+    if (
+      !originCity ||
+      !destinationCity ||
+      !departureDate ||
+      !passengersCount ||
+      !seatClass
+    ) {
+      return res.status(400).json({
+        status: false,
+        message:
+          "Field 'from', 'to', 'departure', 'passengers', and 'seat_class' are required",
+        data: null
+      });
+    }
+
     try {
       const tickets = await prisma.ticket.findMany({
+        where: {
+          airplaneSeatClass: {
+            type: seatClass
+          },
+          flight: {
+            departureTime: {
+              gt: new Date(departureDate)
+            },
+            departureAirport: {
+              city: {
+                cityIata: originCity
+              }
+            },
+            arrivalAirport: {
+              city: {
+                cityIata: destinationCity
+              }
+            }
+          }
+        },
         include: {
+          airplaneSeatClass: {
+            include: {
+              airplane: {
+                include: {
+                  airline: true
+                }
+              }
+            }
+          },
           flight: {
             include: {
               arrivalAirport: {
