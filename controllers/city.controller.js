c1d48cc0bf3508053d56fc07d80bca40c7cc521b
Rama Astra <ramaastra333@gmail.com>
2024-05-31 19:54:52 +0800

    feat: add city search endpoint to find flight destination

---
+++ controllers/city.controller.js
@@ -0,0 +1,30 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+module.exports = {
+  getAll: async (req, res, next) => {
+    try {
+      const { search: searchKeyword } = req.query;
+      const cities = await prisma.city.findMany(
+        searchKeyword
+          ? {
+              where: {
+                name: {
+                  contains: searchKeyword,
+                  mode: 'insensitive'
+                }
+              }
+            }
+          : {}
+      );
+
+      res.status(200).json({
+        status: true,
+        message: 'Cities fetched',
+        data: cities
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+};
