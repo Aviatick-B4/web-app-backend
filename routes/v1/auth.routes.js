10590e691cbbc19261e20b50ab5009c519321e78
rizkirobawa <rizkirobawa@gmail.com>
2024-06-10 21:04:00 +0700

    feat: add API get all users and by id

--- routes/v1/auth.routes.js
+++ routes/v1/auth.routes.js
@@ -2,12 +2,12 @@ const { Router } = require('express');
 const passport = require('../../libs/passport');
 const auth = require('../../controllers/auth.controller.js');
 const router = Router();
-const {restrict} = require('../../middlewares/auth.middleware');
+const { restrict, isAdmin } = require('../../middlewares/auth.middleware');
 
 router.post('/register', auth.register);
 router.post('/verify-otp', auth.verifyOtp);
 router.post('/resend-otp', auth.resendOtp);
-router.delete('/users/:id', auth.deleteUser);
+router.delete('/users/:id', restrict, auth.deleteUser);
 
 router.post('/login', auth.login);
 router.get(
@@ -26,5 +26,7 @@ router.get(
 router.post('/forgot-password', auth.sendResetPasswordEmail);
 router.post('/reset-password', auth.resetPassword);
 router.get('/verified', restrict, auth.verified);
+router.get('/users', restrict, isAdmin, auth.getAll);
+router.get('/users/:id', restrict, auth.getUserById);
 
 module.exports = router;
