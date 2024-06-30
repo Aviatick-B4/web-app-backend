cdf12aaf43a34ae84a605e2ea7a935738112ff3d
rizkirobawa <rizkirobawa@gmail.com>
2024-07-01 00:14:13 +0700

    feat: separated booking API

--- routes/v1/booking.routes.js
+++ routes/v1/booking.routes.js
@@ -3,10 +3,14 @@ const {
   booking,
   getAll,
   getDetail,
+  prepareBooking,
+  completeBooking,
 } = require('../../controllers/booking.controllers');
 const { restrict } = require('../../middlewares/auth.middleware');
 
 router.post('/new-booking/:tripType', restrict, booking);
+router.post('/prepare/:tripType', restrict, prepareBooking);
+router.post('/complete', restrict, completeBooking);
 router.get('/booking-history', restrict, getAll);
 router.get('/booking-history/:bookingId', restrict, getDetail);
 
